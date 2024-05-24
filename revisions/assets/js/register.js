94929c24fe6e5f6c49344d1dd05bccc2193638bf
lesly-lodin <lesly.lodin@baobab-ingenierie.fr>
2024-05-24 16:54:23 +0200

    JS Form Day 4

--- revisions/assets/js/register.js
+++ revisions/assets/js/register.js
@@ -14,6 +14,7 @@ document.addEventListener(
                 document.querySelector('#valSal').textContent = this.value + " €";
             }
         );
+
         /**
          * Evénement : changement date de naissance
          */
@@ -21,6 +22,47 @@ document.addEventListener(
             'change',
             function (evt) {
                 document.querySelector('#age').textContent = age(evt.target.value) + (age(this.value) > 1 ? ' ans' : ' an');
+            }
+        );
+
+        /**
+         * Requête AJAX : liste des départements
+         */
+
+        // Etape 1 : instancie l'objet AJAX
+        const xhr = new XMLHttpRequest();
+
+        // Etape 2 : ouvre la requête AJAX
+        xhr.open('get', 'https://geo.api.gouv.fr/departements?fields=nom,code');
+
+        // Etape 3 : envoie la requête AJAX
+        xhr.send();
+
+        // Etape 4 : écoute le retour du serveur
+        xhr.addEventListener(
+            'readystatechange',
+            function () {
+                if (xhr.readyState === 4 && (xhr.status === 0 || xhr.status === 200)) {
+                    const data = JSON.parse(xhr.responseText);
+                    let opt;
+                    data.forEach(function (elt) {
+                        opt = document.createElement('option');
+                        opt.value = elt.code;
+                        opt.textContent = elt.nom;
+                        document.querySelector('#dept').appendChild(opt);
+                    });
+                }
+            }
+        );
+
+        /**
+         * Requête AJAX : liste des villes
+         */
+        document.querySelector('#dept').addEventListener(
+            'change',
+            function () {
+                const resp = fetch('https://geo.api.gouv.fr/departements/' + this.value + '/communes');
+                console.log(resp.json());
             }
         );
     }
